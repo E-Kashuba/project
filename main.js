3008032d70ccc85b0e027af0278230be02c7bd8f
E-Kashuba <62133998+E-Kashuba@users.noreply.github.com>
2021-03-18 10:45:23 +0500

    Привели в порядок main.js

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
-let money = Number( prompt("Ваш бюджет на месяц?") );
-let time = prompt("Введите дату в формате YYYY-MM-DD");
-
-let expensesQuest1 = prompt("Введите обязательную статью расходов в этом месяце");
-let expensesQuest2 = Number( prompt("Во сколько оойдется?") );
+let money = prompt("Ваш бюджет на месяц?", ""),
+    time = prompt("Введите дату в формате YYYY-MM-DD", "");
 
 let appData = {
-    budget: money,
-    timeData: time,
-    expenses: `${expensesQuest1} : ${expensesQuest2}`,
-    optionalExpenses: "",
+    budjet: money,
+    expenses: {},
+    optionalExpenses: {},
     income: [],
+    timeData: time,
     saving: false
 }
 
-alert(money / 30);
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", "");
+let a2 = prompt("Во сколько оойдется?", "");
+let a3 = prompt("Введите обязательную статью расходов в этом месяце", "");
+let a4 = prompt("Во сколько оойдется?", "");
+
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+
+alert(appData.budjet / 30);
